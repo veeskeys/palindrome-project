3d30fee8eb3f3489c1529c92ab6e183d57caeb08
veeskeys <vanessa.m.hyde@gmail.com>
2025-07-26 17:30:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const textInput= document.getElementById("text-input");
+const checkBtn=document.getElementById("check-btn");
+const resultDiv=document.getElementById("result");
+
+function checkPalindrome(str){
+  const cleanString = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  const reverseString = cleanString.split("").reverse().join("");
+  return cleanString === reverseString;
+}
+
+
+function checkInput(){
+const inputValue = textInput.value;
+
+if(inputValue===""){
+  alert("Please input a value")
+};
+
+if (checkPalindrome(inputValue)){
+    resultDiv.textContent = `${inputValue} is a Palindrome.`
+  }
+  else{
+    resultDiv.textContent = `${inputValue} is not a Palindrome.`
+  }
+  };
+
+checkBtn.addEventListener("click",checkInput);
+
+checkBtn.addEventListener("click", checkPalindrome);
